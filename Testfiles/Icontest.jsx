e0ae3da0ac87d44a0923000d5fe1eb7232b233e6
Natnael Guchima <natnaelguchima@Natnaels-MacBook-Air.local>
2023-10-25 14:26:03 +0300

    deleted icontest file used to test icon config

--- Testfiles/Icontest.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
-function icontest(props) {
-  return <MaterialCommunityIcons size={20} color="green" name="email" />;
-}
-
-export default icontest;
