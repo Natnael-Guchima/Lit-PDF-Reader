1db5425ef7f6f18c5857378447f1ef93fba8e1f5
Natnael Guchima <natnaelguchima@Natnaels-MacBook-Air.local>
2023-10-25 15:08:04 +0300

    cleaned app.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+
 import PDFExample from './PDFExample';
 
 const App = () => {
   return <PDFExample />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
