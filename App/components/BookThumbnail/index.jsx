6b821c0164a6dfa69f9f81020760507002b15230
Natnael Guchima <natnaelguchima12@gmail.com>
2023-11-02 15:57:08 +0300

    fixed bookthumbnail height

--- App/components/BookThumbnail/index.jsx
+++ App/components/BookThumbnail/index.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 
 import Pdf from 'react-native-pdf';
+import colors from '../../config/colors';
 
 const mockURI =
   'https://cdn.ttgtmedia.com/rms/pdf/pragmatic_programmer_ch4.pdf';
-const source = {uri: mockURI, cache: true};
-function BookThumbnail(props) {
+
+function BookThumbnail({uri}) {
+  const source = {uri: uri ? uri : mockURI, cache: true};
   return (
     <View style={styles.container}>
       <Pdf
         onLoadComplete={(numberOfPages, path, size, tableOfConentes) =>
-          console.log(numberOfPages, [path, size, tableOfConentes])
+          console.log(numberOfPages, path, size, tableOfConentes, 'info')
         }
         source={source}
         style={styles.pdf}
@@ -29,6 +31,7 @@ const styles = StyleSheet.create({
   pdf: {
     width: '100%',
     height: 300,
+    backgroundColor: colors.white,
   },
   container: {width: '50%'},
 });
--- App/components/Screen/index.jsx
+++ App/components/Screen/index.jsx
@@ -11,6 +11,11 @@ function Screen({props}) {
     <SafeAreaView style={styles.screen}>
       <NavigationBar />
       <BookThumbnail />
+      <BookThumbnail
+        uri={
+          'https://kolegite.com/EE_library/books_and_lectures/%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%B8%D1%80%D0%B0%D0%BD%D0%B5/Clean%20Code%20-%20%20A%20Handbook%20of%20Agile%20Software%20Craftsmanship.pdf'
+        }
+      />
     </SafeAreaView>
   );
 }
