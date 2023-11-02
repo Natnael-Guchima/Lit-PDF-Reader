794a348ca17fe660e48eaa79419eb1652d6e1bba
Natnael Guchima <natnaelguchima12@gmail.com>
2023-11-02 17:11:48 +0300

    built basic flat list for grid component

--- App/layouts/BookGrid/index.jsx
+++ App/layouts/BookGrid/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
+
+import BookThumbnail from '../../components/BookThumbnail';
+
+const books = [
+  {uri: 'https://cdn.ttgtmedia.com/rms/pdf/pragmatic_programmer_ch4.pdf'},
+  {
+    uri: 'https://kolegite.com/EE_library/books_and_lectures/%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%B8%D1%80%D0%B0%D0%BD%D0%B5/Clean%20Code%20-%20%20A%20Handbook%20of%20Agile%20Software%20Craftsmanship.pdf',
+  },
+];
 function BookGrid(props) {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={books}
+        renderItem={({item}) => <BookThumbnail uri={item.uri} />}
+      />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   container: {
