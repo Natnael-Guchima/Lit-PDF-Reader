6cee52eda1ccb15fe7e474f95d66a8b3eab2d01a
Natnael Guchima <natnaelguchima12@gmail.com>
2023-11-03 13:47:23 +0300

    created pdf reader file

---
+++ App/pages/PDFReader/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+function PDFReader(props) {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'blue',
+    flex: 1,
+  },
+});
+export default PDFReader;
