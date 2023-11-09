557a5c95da70c39b0330f6098cf0705d3a3ec0a4
Natnael Guchima <natnaelguchima12@gmail.com>
2023-11-09 10:27:42 +0300

    created a basic file for drawer naigation header

---
+++ App/components/DrawerHeader/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+function DrawerHeader(props) {
+  return <View style={styles.container} />;
+}
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    backgroundColor: 'blue',
+  },
+});
+export default DrawerHeader;
