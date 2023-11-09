6fefa85cd146d49fd15cbf61ae14226ee8b8327c
Natnael Guchima <natnaelguchima12@gmail.com>
2023-11-09 10:43:04 +0300

    built drawer header ui

--- App/components/DrawerHeader/index.jsx
+++ App/components/DrawerHeader/index.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
+
+import colors from '../../config/colors';
+import MenuIon from '../MenuIcon';
+import HeaderText from '../HeaderText';
+
 function DrawerHeader(props) {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <MenuIon name="arrow-left" size={20} color={colors.white} />
+      <HeaderText style={styles.headerText}>pdf reader title</HeaderText>
+      <MenuIon name="table-of-contents" size={20} color={colors.white} />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   container: {
     height: 60,
-    backgroundColor: 'blue',
+    backgroundColor: colors.primary,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  headerText: {
+    flex: 1,
+    marginLeft: 20,
   },
 });
 export default DrawerHeader;
