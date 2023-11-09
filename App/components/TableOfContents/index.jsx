31c97c8547888f8f6ee4a49795c2497b0d6fa134
Natnael Guchima <natnaelguchima12@gmail.com>
2023-11-09 14:38:34 +0300

    added header to table of contents

--- App/components/TableOfContents/index.jsx
+++ App/components/TableOfContents/index.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
 
+import HeaderText from '../HeaderText';
+import colors from '../../config/colors';
+
 function TableOfContents(props) {
   return (
     <DrawerContentScrollView {...props}>
+      <HeaderText style={{color: colors.black, marginLeft: 10}}>
+        Table of Contents
+      </HeaderText>
       <DrawerItem label={'teest'} onPress={() => console.log('link pressed')} />
     </DrawerContentScrollView>
   );
