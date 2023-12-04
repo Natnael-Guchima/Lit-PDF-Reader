bdba5588fd8238fc8b05bb9374856fa7845468c5
Natnael Guchima <natnaelguchima12@gmail.com>
2023-12-04 15:05:51 +0300

    added plugin to view redux state

--- App/components/TableOfContents/index.jsx
+++ App/components/TableOfContents/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+import {useDispatch} from 'react-redux';
 
 import HeaderText from '../HeaderText';
 import colors from '../../config/colors';
+import {updatePageNumber} from '../../services/slices/pdfSlice';
 
 function TableOfContents({tableOfContents, navigation, ...props}) {
+  const dispatch = useDispatch();
   return (
     <DrawerContentScrollView {...props}>
       <HeaderText style={{color: colors.black, marginLeft: 10}}>
@@ -16,6 +19,7 @@ function TableOfContents({tableOfContents, navigation, ...props}) {
           key={`${pageIdx}${title}`}
           label={title}
           onPress={() => {
+            dispatch(updatePageNumber({pageNumber: pageIdx}));
             navigation.closeDrawer();
           }}
         />
--- App/services/store.js
+++ App/services/store.js
@@ -1,3 +1,31 @@
 import {configureStore} from '@reduxjs/toolkit';
 import pdfSlice from './slices/pdfSlice';
-export default store = configureStore({reducer: {pdfSlice}});
+
+// const middlewares = getDefaultMiddleware({
+//   // https://github.com/reduxjs/redux-toolkit/issues/415
+//   immutableCheck: false,
+// });
+
+// if (__DEV__) {
+//   const createDebugger = require('redux-flipper').default;
+//   middlewares.push(createDebugger());
+// }
+
+const middlewares = [
+  /* other middlewares */
+];
+
+if (__DEV__) {
+  const createDebugger = require('redux-flipper').default;
+
+  middlewares.push(createDebugger());
+}
+
+export default store = configureStore({
+  reducer: {pdfSlice},
+  middleware: getDefaultMiddleware => {
+    const result = getDefaultMiddleware().concat(middlewares);
+
+    return result;
+  },
+});
