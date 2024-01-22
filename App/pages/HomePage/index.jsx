a56a59385f3e8dd9ab16d0a19f12beed371e95cf
Natnael Guchima <natnaelguchima12@gmail.com>
2024-01-23 01:08:07 +0300

    started navigating to pdf reader page on launch

--- App/pages/HomePage/index.jsx
+++ App/pages/HomePage/index.jsx
@@ -6,40 +6,23 @@ import BookGrid from '../../layouts/BookGrid';
 import BookThumbnail from '../../components/BookThumbnail';
 import {CONST} from '../../config/CONST';
 
-const getInitialURL = async navigation => {
-  //get app state first based on that chose a listener type check if the app is launched by intent first
-  console.log('get initial url');
-  const appLaunchURL = await Linking.getInitialURL();
-  if (appLaunchURL !== null) {
-    //app is launched for first time
-    console.log('app was launched', appLaunchURL);
-  } else {
-    console.log('if not first launch');
-    Linking.addEventListener('url', event => {
-      //app pops up from background
-      console.log('app was in backgournd', event.url);
-      event.url &&
-        navigation.push(CONST.ROUTES.PDF_READER_PAGE, {
-          uri: 'https://thepfengineerdotcom.files.wordpress.com/2016/05/notes-the-practicing-mind.pdf',
-        });
-    });
-  }
-
-  // Linking.addEventListener('url', event => {
-  //   console.log(event, 'event value');
-  // });
-};
-const getURL = async () => {
+const getURL = async navigation => {
   const URL = await Linking.getInitialURL();
-  console.log(URL, 'on launch URL');
+  URL &&
+    navigation.navigate(CONST.ROUTES.PDF_READER_PAGE, {
+      uri: 'https://thepfengineerdotcom.files.wordpress.com/2016/05/notes-the-practicing-mind.pdf',
+    });
 };
 
 function HomePage({navigation}) {
   useEffect(() => {
     Linking.addEventListener('url', event => {
-      console.log(event, 'listener');
+      event.url &&
+        navigation.navigate(CONST.ROUTES.PDF_READER_PAGE, {
+          uri: 'https://thepfengineerdotcom.files.wordpress.com/2016/05/notes-the-practicing-mind.pdf',
+        });
     });
-    getURL();
+    getURL(navigation);
   }, []);
 
   return (
